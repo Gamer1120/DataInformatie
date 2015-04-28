eaaa4f8dbd022c29dd37f78e3b2915139484cc8d
meteoorkip <svenkonings@live.nl>
2015-04-28 15:09:02 +0200

    betere result

--- src/main/java/week2/JDBC.java
+++ src/main/java/week2/JDBC.java
@@ -1,6 +1,7 @@
 package week2;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,14 +9,6 @@ import java.sql.SQLException;
 
 public class JDBC {
 	public static void main(String[] args) {
-		new JDBC();
-	}
-
-	public JDBC() {
-		init();
-	}
-
-	public void init() {
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
@@ -25,14 +18,25 @@ public class JDBC {
 		String pass = "kip";
 		String dbName = "db02";
 		String url = "jdbc:postgresql://" + host + ":5432/" + dbName;
+		String sql = "SELECT name, year FROM movie WHERE rating >= 8.7 AND rating <= 9.0";
 		try {
 			Connection connection = DriverManager.getConnection(url, dbName,
 					pass);
 			Statement statement = connection.createStatement();
-			ResultSet result = statement.executeQuery("SELECT name, year FROM movie WHERE rating >= 8.7 AND rating <= 9.0");
+			ResultSet result = statement.executeQuery(sql);
+			ResultSetMetaData data = result.getMetaData();
+			int columns = data.getColumnCount();
+			while (result.next()) {
+				for (int i = 1; i <= columns; i++) {
+				System.out.print(data.getColumnName(i) + ": " + result.getObject(i) + " ");
+				}
+				System.out.println();
+			}
+			connection.close();
+			statement.close();
+			result.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
