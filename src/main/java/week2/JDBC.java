76f3ef99810a1993226d1edf3553d639545dd376
Gamer1120 <michaelkoopmantest@gmail.com>
2015-04-28 16:24:03 +0200

    Inefficient

--- src/main/java/week2/JDBC.java
+++ src/main/java/week2/JDBC.java
@@ -1,18 +1,34 @@
 package week2;
 
+import java.awt.List;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class JDBC {
+
+	String host = "castle.ewi.utwente.nl";
+	String pass = "kip";
+	String dbName = "db02";
+	String url = "jdbc:postgresql://" + host + ":5432/" + dbName;
+	Connection connection;
+	Statement statement;
+
 	public static void main(String[] args) {
 		new JDBC();
 	}
 
 	public JDBC() {
+		try {
+			connection = DriverManager.getConnection(url, dbName, pass);
+			statement = connection.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		init();
 	}
 
@@ -22,28 +38,68 @@ public class JDBC {
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error loding driver: " + e);
 		}
-		String host = "castle.ewi.utwente.nl";
-		String pass = "kip";
-		String dbName = "db02";
-		String url = "jdbc:postgresql://" + host + ":5432/" + dbName;
-		try {
-			Connection connection = DriverManager.getConnection(url, dbName,
-					pass);
 
-			String query = "SELECT moviesofactor(?)";
-			PreparedStatement statement = connection.prepareStatement(query);
-			statement.setString(1, "Bruce Willis");
-			statement.setFetchSize(5);
-			int iters = 100;
-			long startTime = System.currentTimeMillis();
-			for (int i = 0; i < iters; i++) {
-				ResultSet result = statement.executeQuery();
+		int iters = 5;
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < iters; i++) {
+			for (Integer mid : alleFilms()) {
+				if (heeftRegisseur(mid)) {
+					ArrayList<String> aut = auteurVanFilm(mid);
+				}
 			}
-			System.out.println((System.currentTimeMillis() - startTime) / (1.0
-					* iters));
-			connection.close();
+		}
+		System.out.println((System.currentTimeMillis() - startTime)
+				/ (1.0 * iters));
+
+	}
+
+	public ArrayList<Integer> alleFilms() {
+		ArrayList<Integer> retList = new ArrayList<Integer>();
+		ResultSet res = null;
+		try {
+			res = statement.executeQuery("SELECT m.mid FROM movie m");
+			while (res.next()) {
+				retList.add(res.getInt(1));
+			}
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return retList;
 	}
+
+	public ArrayList<String> auteurVanFilm(int mid) {
+		ArrayList<String> retList = new ArrayList<String>();
+		ResultSet res = null;
+		try {
+			res = statement
+					.executeQuery("SELECT p.name FROM person p, writes w WHERE p.pid = w.pid AND w.mid = "
+							+ mid);
+			while (res.next()) {
+				retList.add(res.getString(1));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return retList;
+	}
+
+	public boolean heeftRegisseur(int mid) {
+		ResultSet res = null;
+		boolean result = true;
+		try {
+			res = statement
+					.executeQuery("SELECT * FROM directs WHERE directs.mid = "
+							+ mid);
+			while (res.next()) {
+				result = false;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
 }
